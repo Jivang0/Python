e894a704ab09ee7aec0fdeb06da4e01127649f3a
Jivang0 <133135655+Jivang0@users.noreply.github.com>
2025-08-26 14:53:04 +0545

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,25 @@
+# tuple is an ordered sequence of item same as a list.
+# it is defined within parenteses() where items
+# are separated by commas.
+#tuple cannot support updation
+
+# t = (10,20,"hellow")
+# print(t,type(t))
+
+# t =(11)
+# print(t)
+
+# t = (1,2,"hii",3,4)
+# print(type(t))
+
+# l=["hii",1,"apple",0.3]
+
+# print(type(l))
+
+d={"name":"Ram","age":20}
+
+print(type(d))
+
+set = {1,2,3,4}
+
+print(type(set))
